fc5eeda039de3d4ac427c007f0e6ef6466bfc310
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-23 14:19:16 -0700

    adds comment#show and new comment form to posts#show, need to delete comments boolean from post model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
   def create
+    @post = Post.find(params[:comment][:post_id])
+    @comment = @post.comments.build(params[:post])
+    @comment.user = current_user
+    @comment.save
+    redirect_to @post
   end
 
   def edit
