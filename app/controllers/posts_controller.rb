c8d7ce3669e17edbc81939eb1a2f1fd865190583
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-23 13:31:37 -0700

    adds basic posts #new, #show and #index views. views and controllers are currently unrestricted

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = current_user.posts.build
   end
 
   def create
+    @post = current_user.posts.build(params[:post])
+    @post.save
+    redirect_to posts_path
   end
 
   def edit
