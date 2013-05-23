2b407afd51840637c7d2bb745959e508b9f2756e
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-23 15:56:02 -0700

    adds contextual edit user link in navbar and admin delete link in user#show, access is unrestricted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
+  
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+    redirect_to @user
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+  
 end
