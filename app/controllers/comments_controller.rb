77aa48ce08fc7b6b5282dcf0820f0f6e268e5160
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-23 14:50:32 -0700

    adds edit and delete links to next to comments, access is unrestricted

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,11 +9,19 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    @comment.update_attributes(body: params[:comment][:body])
+    redirect_to post_path(@comment.post)
   end
 
-  def delete
+  def destroy
+    @comment = Comment.find(params[:id])
+    @post = @comment.post
+    @comment.destroy
+    redirect_to @post
   end
 end
