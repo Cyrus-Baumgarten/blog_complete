0fb9d742ead927dcba3ebca4933c3e8088b4cf2d
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-23 15:06:09 -0700

    restricts access of comment editing and deletion to admins and the makers of the comment both in the views and the controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   
+  before_filter :authenticate_user!, only: [ :create, :edit, :update, :destroy ]
+  
   def create
     @post = Post.find(params[:comment][:post_id])
     @comment = @post.comments.build(body: params[:comment][:body])
@@ -10,18 +12,32 @@ class CommentsController < ApplicationController
 
   def edit
     @comment = Comment.find(params[:id])
+    unless user_signed_in? and (current_user.admin? or current_user == @comment.user)
+      flash[:error] = "You are not authorized to do that"
+      redirect_to root_path
+    end
   end
 
   def update
     @comment = Comment.find(params[:id])
-    @comment.update_attributes(body: params[:comment][:body])
-    redirect_to post_path(@comment.post)
+    if user_signed_in? and (current_user.admin? or current_user == @comment.user)
+      @comment.update_attributes(body: params[:comment][:body])
+      redirect_to post_path(@comment.post)
+    else
+      flash[:error] = "You are not authorized to do that"
+      redirect_to root_path
+    end
   end
 
   def destroy
     @comment = Comment.find(params[:id])
-    @post = @comment.post
-    @comment.destroy
-    redirect_to @post
+    if user_signed_in? and (current_user.admin? or current_user == @comment.user)
+      @post = @comment.post
+      @comment.destroy
+      redirect_to @post
+    else
+      flash[:error] = "You are not authorized to do that"
+      redirect_to root_path
+    end
   end
 end
