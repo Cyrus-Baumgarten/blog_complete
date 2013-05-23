4f144be3774f2917b250e4dde5f1f97cb383736f
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-23 16:11:21 -0700

    restricts access to manipulating users in views and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,42 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]) 
+    unless current_user == @user or current_user.admin?
+      if @user.anonymous?
+        flash[:error] = "This users profile is private"
+        redirect_to root_path
+      end
+    end
   end
 
   def edit
     @user = User.find(params[:id])
+    unless current_user == @user or current_user.admin?
+      flash[:error] = "You are not authorized to do that"
+      redirect_to root_path
+    end
   end
   
   def update
     @user = User.find(params[:id])
-    @user.update_attributes(params[:user])
-    redirect_to @user
+    if current_user == @user or current_user.admin?
+      @user.update_attributes(params[:user])
+      redirect_to @user
+    else
+      flash[:error] = "You are not authorized to do that"
+      redirect_to root_path
+    end
   end
   
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
-    redirect_to root_path
+    if current_user == @user or current_user.admin?    
+      @user.destroy
+      redirect_to root_path
+    else
+      flash[:error] = "You are not authorized to do that"
+      redirect_to root_path
+    end
   end
-  
 end
