65633d54abad732f909099c0815b740f4511c582
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-25 03:49:44 -0700

    reconfigures database, updates user validation to allow nil name

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :anonymous, :image_url, :subscribe
   # attr_accessible :title, :body
   
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, allow_nil: true
   
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
