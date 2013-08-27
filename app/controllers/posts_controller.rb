fbe8e3c477723f31913edba8bb9534d181d27f98
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-08-27 15:01:14 -0700

    updates about and title

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   
   before_filter :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
-  
+    
   def index
     @posts = Post.all
   end
